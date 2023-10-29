549b0643719bd0a7538409eb3fc7995de8e10a40
Mateo Barbachan <112145980+teozz17@users.noreply.github.com>
2023-10-28 21:05:09 -0300

    Error messagges

---
+++ src/components/Error/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default () => {
+    return (
+        <div className="error">
+            <h1 className="error__title">User or Password Incorrect</h1>
+        </div>
+    );
+}
--- src/containers/Login/index.jsx
+++ src/containers/Login/index.jsx
@@ -1,45 +1,58 @@
 import React, {useState, useEffect} from "react";
 import {useForm} from "react-hook-form";
+import { ErrorMessage } from '@hookform/error-message';
 
+import Error from '../../components/Error';
 import getUsers from '../../api/users';
 
 import "./index.scss";
 
 const Login = () => {
 
-const {register, handleSubmit} = useForm();
+const {register, formState: { errors }, handleSubmit} = useForm();
 
 const [users, setUsers] = useState([]); 
 
 const init = async () => {
-    const users = await getUsers();
-    console.log(users);
-    setUsers(users);
+    const response = await getUsers();
+    setUsers(response.data);
+    console.log(response.data);
 }
 
 useEffect(() => {
     init();
 }, []);
 
-const onSubmit = (data, e) => console.log(data);
+const onSubmit = (data, e) => {
+    const user = users.find(user => user.email === data.email && user.password === data.password);
+    if (user === undefined) {
+        console.log("Usuario no encontrado");
+    } else{
+        console.log("Usuario encontrado");
+    }
+};
 
   return (
     <div className="login">
         <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
-            <h1 className="login-title">Cart App</h1>
+          
+            <h1 className="login-form__title">Cart App</h1>
             <input
-                {...register ("email", {required: true})} 
+                {...register ("email", {required: "⚠ This field is required!!!"})} 
                 className="login-form__input" 
-                type="email" 
-                placeholder="Enter your email" />
+                type="text" 
+                placeholder="Enter your email" 
+            />
+            <ErrorMessage errors={errors} name="email" as={<p className="login-form__error"></p>}/>
             <input
-                {...register ("password", {required: true})} 
+                {...register ("password", {required: "⚠ This field is required!!!"})} 
                 className="login-form__input" 
                 type="password" 
-                placeholder="Enter your password" />
+                placeholder="Enter your password" 
+            />
+            <ErrorMessage errors={errors} name="password" as={<p className="login-form__error"></p>}/>
             <button type="submit" className="login-form__button">Login</button>
         </form>
-        
         <div className="wave" ></div>
     </div>
   )
