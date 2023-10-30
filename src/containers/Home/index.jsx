1dc2ebc07bbbf7e6c5f652f29cc22a80f90e86b0
Mateo Barbachan <112145980+teozz17@users.noreply.github.com>
2023-10-30 20:46:52 -0300

    home and css fix

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import ProductList from 'Components/ProductList';
+import { getProducts } from '../../api/products';
 
 import './index.scss';
 
-const Home = () => (
-  <div className="home">
-    <h1>
-      Welcome to the Home Page of the React Bootcamp App
-    </h1>
-  </div>
-);
+const Home = () => {
+  
+  const [products, setProducts] = useState([]);
+
+  const init = async () => {
+    try {
+      const response = await getProducts();
+      setProducts(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.log("ERROR");
+    }
+  }
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  return(
+    <div className="home">
+      <h1>
+        Welcome to the Home Page of the React Bootcamp App
+      </h1>
+      <ProductList products={products} />
+    </div>
+  );
+};
 
 export default Home;
