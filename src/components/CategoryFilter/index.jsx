b5f4cb89c1e6829cd7f7be3bd4a4fb142824ba46
Mateo Barbachan <112145980+teozz17@users.noreply.github.com>
2023-10-31 19:28:28 -0300

    category filter refactor

--- src/components/CategoryFilter/index.jsx
+++ src/components/CategoryFilter/index.jsx
@@ -10,14 +10,21 @@ const CategoryFilter = ({categories, setActualCategory, actualCategory}) => {
 
     return (
         <div className="category-filter">
-            <div className="category-filter__icon" onClick={() => {
-                        setCategoriesMenuOpen(!categoriesMenuOpen);
-                    }} >
+            <div 
+                className="category-filter__icon"
+                onClick={() => setCategoriesMenuOpen(true)}
+                onMouseEnter={() => setCategoriesMenuOpen(true)}
+                onMouseLeave={() => setCategoriesMenuOpen(false)}
+            >
                 <AiFillFilter 
                     className="category-filter__selector" 
                 /> 
             </div>
-            <div className={ categoriesMenuOpen ? "category-filter__container__open" : "category-filter__container"}>
+            <div 
+                className={ categoriesMenuOpen ? "category-filter__container__open" : "category-filter__container"} 
+                onMouseEnter={() => setCategoriesMenuOpen(true)}
+                onMouseLeave={() => setCategoriesMenuOpen(false)}
+            >
                 <ul className={ categoriesMenuOpen ? "category-filter__container__list__open" : "category-filter__container__list"}>
                     {categories.map((category) => (
                         <li key={category}>
