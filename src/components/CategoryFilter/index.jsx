4a9ee944c693503fa111ea1399c9d6dcfbdb2410
Mateo Barbachan <112145980+teozz17@users.noreply.github.com>
2023-10-31 19:20:25 -0300

    category filter refactor

--- src/components/CategoryFilter/index.jsx
+++ src/components/CategoryFilter/index.jsx
@@ -17,18 +17,20 @@ const CategoryFilter = ({categories, setActualCategory, actualCategory}) => {
                     className="category-filter__selector" 
                 /> 
             </div>
-            <ul className={ categoriesMenuOpen ? "category-filter__list__open" : "category-filter__list"}>
-                {categories.map((category) => (
-                    <li key={category}>
-                        <CategoryButton
-                            isDisabled={actualCategory === category}
-                            key={category}
-                            name={category}
-                            onCLick={() => setActualCategory(category)}
-                        />
-                    </li>
-                ))}
-            </ul>
+            <div className={ categoriesMenuOpen ? "category-filter__container__open" : "category-filter__container"}>
+                <ul className={ categoriesMenuOpen ? "category-filter__container__list__open" : "category-filter__container__list"}>
+                    {categories.map((category) => (
+                        <li key={category}>
+                            <CategoryButton
+                                isDisabled={actualCategory === category}
+                                key={category}
+                                name={category}
+                                onCLick={() => setActualCategory(category)}
+                            />
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 }
