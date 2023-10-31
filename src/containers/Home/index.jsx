d7096507a226d3279050ed1092fa016842c1d061
Mateo Barbachan <112145980+teozz17@users.noreply.github.com>
2023-10-31 17:57:21 -0300

    home container

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,33 +1,65 @@
 import React, { useState, useEffect } from 'react';
 import ProductList from 'Components/ProductList';
-import { getProducts } from '../../api/products';
+import { getProducts, getProductsByCategory, getCategories } from '../../api/products';
+import CategoryFilter from 'Components/CategoryFilter';
 
 import './index.scss';
 
 const Home = () => {
   
   const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [actualCategory, setActualCategory] = useState("All");
 
   const init = async () => {
+    getTheCategories();
+    getTheProducts();
+  }
+
+  const getTheCategories = async () => {
     try {
-      const response = await getProducts();
-      setProducts(response.data);
-      console.log(response.data);
+      const response = await getCategories();
+      response.data.unshift("All");
+      setCategories(response.data);
     } catch (error) {
       console.log("ERROR");
     }
   }
 
+  const getTheProducts = async () => {
+    if (actualCategory === "All") {
+      try {
+        const response = await getProducts();
+        setProducts(response.data);
+      } catch (error) {
+        console.log("ERROR");
+      }
+    } else {
+      try {
+        const response = await getProductsByCategory(actualCategory);
+        setProducts(response.data);
+      } catch (error) {
+        console.log("ERROR");
+      }
+    }
+  }
+
+  const handleCategory = (category) => {
+    setActualCategory(category);
+  }
+
   useEffect(() => {
     init();
-  }, []);
+  } , [actualCategory]);
 
   return(
     <div className="home">
-      <h1>
-        Welcome to the Home Page of the React Bootcamp App
-      </h1>
-      <ProductList products={products} />
+        <CategoryFilter 
+          categories={ categories } 
+          setActualCategory={ handleCategory } 
+          actualCategory={ actualCategory } 
+        />
+      <ProductList products={ products } />
     </div>
   );
 };
