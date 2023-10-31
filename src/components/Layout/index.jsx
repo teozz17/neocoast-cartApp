f04a1b94c2b4784de0ccb4e66e866a2b4b71f411
Mateo Barbachan <112145980+teozz17@users.noreply.github.com>
2023-10-31 17:56:30 -0300

    layout refactor to allow change on log in

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -2,25 +2,34 @@ import React from "react";
 import { Outlet } from "react-router";
 import { ROUTES } from "../../data/constants";
 import NavBar from "Components/NavBar";
-import { BsFillCartPlusFill } from "react-icons/bs";
-import { BsGiftFill } from "react-icons/bs";
+import { BsFillCartPlusFill, BsGiftFill } from "react-icons/bs";
 import { CgProfile } from "react-icons/cg";
 
 import "./index.scss";
 
 const Layout = () => {
+
+    const actualUser = JSON.parse(localStorage.getItem("userData"));
+
     return (
         <div className="layout">
-            <NavBar 
-                logo={'https://img.freepik.com/vector-gratis/diseno-logotipo-tienda-instagram_23-2149750724.jpg'}
-                routes={[
-                    {label: 'Cart', logo: <BsFillCartPlusFill />, route: ROUTES.cart}, 
-                    {label: 'Gift', logo: <BsGiftFill />, route: ROUTES.gitf}, 
-                    {label: 'Progile', logo: <CgProfile />, route: ROUTES.profile},
+            {(actualUser) != null ?
+                <NavBar 
+                    context = {actualUser}
+                    logo={'https://img.freepik.com/vector-gratis/diseno-logotipo-tienda-instagram_23-2149750724.jpg'}
+                    routes={[
+                        {label: 'Cart', logo: <BsFillCartPlusFill />, route: ROUTES.cart}, 
+                        {label: 'Gift', logo: <BsGiftFill />, route: ROUTES.gitf}, 
+                        {label: 'Progile', logo: <CgProfile />, route: ROUTES.profile},
                 ]}
-            />
+                />
+            :   <NavBar 
+                    logo={'https://img.freepik.com/vector-gratis/diseno-logotipo-tienda-instagram_23-2149750724.jpg'}
+                    routes={[]}
+                />
+            }
             <div className="layout__children">
-                <Outlet/>
+                <Outlet context={actualUser}/>
             </div>
         </div>
     );
