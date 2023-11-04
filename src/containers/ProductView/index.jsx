b8fc75b99918fd5ac0bb541d7b8b5977afadc1f7
Mateo Barbachan <112145980+teozz17@users.noreply.github.com>
2023-11-04 13:58:03 -0300

    Error handling

--- src/containers/ProductView/index.jsx
+++ src/containers/ProductView/index.jsx
@@ -1,28 +1,48 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams, Link } from "react-router-dom";
-import { ROUTES } from "Data/constants";
 import { getProduct } from "../../api/products";
 import ShowElement from "Components/ShowElement";
 import { MoonLoader  } from  'react-spinners'
+import Error from 'Components/Error';
+import { GiRun } from "react-icons/gi";
 
 import "./index.scss";
+import { ErrorResponse } from "@remix-run/router";
 
 const ProductView = () => {
 
     const navigate = useNavigate();
     const { id } = useParams();
     const [product, setProduct] = useState(null);
+    const [productDontExist, setProductDontExist] = useState(false);
     const [loading, setLoading] = useState(true);
+    const [reject, setReject] = useState(false);
 
     const init = async () => {
         try {
             setLoading(true);
             const response = await getProduct(id);
             setProduct(response.data);
-            setLoading(false);
+            let random = Math.floor(Math.random() * 10);
+            if (random < 3) {
+                throw new Error(505, 'Error on product fetch');
+            }
+            if (response.data === '') {
+                throw new ErrorResponse(404, 'Product not found')
+            }
+            setProductDontExist(false);
+            setReject(false);
         } catch (error) {
-            console.log("ERROR ON USER FETCH");
+            switch (true) {
+                case (error instanceof ErrorResponse):
+                    setProductDontExist(true);
+                    break;
+                default:
+                    setReject(true);
+                    break;
+            }
         }
+        setLoading(false);
     }
 
     useEffect(() => {
@@ -36,10 +56,36 @@ const ProductView = () => {
             color="#ff9b00"
             size={99}
             speedMultiplier={0.7}
-            cssOverride={{'marginLeft': "auto", 'marginRight': "auto", 'marginTop': "17.2%"}}
+            cssOverride={{'marginLeft': "auto", 'marginRight': "auto", 'marginTop': "15%"}}
             />
-            : 
-            <div className="product-view">
+            : reject?
+            <div style={{width: "100%"}}> 
+                <div className="product-view__back">
+                        <Link onClick={() => navigate(-1)}>🡸 Back</Link>
+                </div>
+                <div className='product-view-error'>
+                    <Error
+                        color={"black"}
+                        message={"Error on product fetch, please try again"} 
+                        icon={<GiRun className='home-error__icon'/>}
+                    />
+                </div>
+            </div>  
+            : productDontExist?
+            <div style={{width: "100%"}}> 
+                <div className="product-view__back">
+                        <Link onClick={() => navigate(-1)}>🡸 Back</Link>
+                </div>
+                <div className='product-view-error'>
+                    <Error
+                        color={"black"}
+                        message={"Product not found"} 
+                        icon={<GiRun className='home-error__icon'/>}
+                    />
+                </div>
+            </div>  
+            :
+            <div style={{width: '100%'}}>
                 <div className="product-view__back">
                     <Link onClick={() => navigate(-1)}>🡸 Back </Link>
                 </div>
--- src/containers/Profile/index.jsx
+++ src/containers/Profile/index.jsx
@@ -4,8 +4,10 @@ import { ROUTES } from "Data/constants";
 import { getUser } from "../../api/users";
 import ShowElement from "Components/ShowElement";
 import { MoonLoader  } from  'react-spinners'
+import Error from 'Components/Error';
+import { GiRun } from "react-icons/gi";
 
-import index from "./index.scss";
+import "./index.scss";
 
 const Profile = () => {
 
@@ -13,6 +15,7 @@ const Profile = () => {
     const navigate = useNavigate();
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
+    const [reject, setReject] = useState(false);
 
     const init = async () => {
         try {
@@ -20,8 +23,13 @@ const Profile = () => {
             const response = await getUser(actualUser);
             setUser(response.data);
             setLoading(false);
+            let random = Math.floor(Math.random() * 10);
+            if (random < 3){
+                throw new Error();
+            }
+            setReject(false);
         } catch (error) {
-            console.log("ERROR ON USER FETCH");
+            setReject(true);
         }
     
     }
@@ -39,9 +47,22 @@ const Profile = () => {
                     color="#ff9b00"
                     size={99}
                     speedMultiplier={0.7}
-                    cssOverride={{'marginLeft': "auto", 'marginRight': "auto", 'marginTop': "17.2%"}}
+                    cssOverride={{'marginLeft': "auto", 'marginRight': "auto", 'marginTop': "15%"}}
                 />
-            : 
+            : reject?
+            <div style={{width: '100%'}}> 
+                <div className="profile__back">
+                        <Link onClick={() => navigate(-1)}>🡸 Back</Link>
+                </div>
+                <div className='profile-error'>
+                    <Error
+                        color={"black"}
+                        message={"Error on profile fetch, please try again"} 
+                        icon={<GiRun className='home-error__icon'/>}
+                    />
+                </div>
+            </div>  
+            :
             <div className="profile">
                 <div className="profile__back">
                     <Link onClick={() => navigate(-1)}>🡸 Back</Link>
