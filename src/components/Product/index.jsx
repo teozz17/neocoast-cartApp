a71956a48f9d78f56160d11a229b093cc13c51d7
Mateo Barbachan <112145980+teozz17@users.noreply.github.com>
2023-11-05 14:55:05 -0300

    product refactor

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -6,13 +6,30 @@ import "./index.scss";
 
 const Product = ({product}) => {
         
+    const {id, title, image, price, quantity} = product;
+
     return(
-        <div className="product">
-            <Link to={`/product/${product.id}`} className="link">
-                <img className="product__image" src={product.image} alt={product.title} />
-                <h3 className="product__title">{product.title}</h3>
-                <p className="product__price">${product.price}</p>
-            </Link>
+        <div>
+            {!quantity &&
+            <div className="product"> 
+                <Link to={`/product/${id}`} className="link">
+                    <img className="product__image" src={image} alt={title} />
+                    <h3 className="product__title">{title}</h3>
+                    <p className="product__price">${price}</p>
+                </Link>
+            </div>}
+            {quantity &&
+            <div className="cart">
+                <div className="cart-product">
+                    <img className="cart-product__image" src={image} alt={title} />
+                    <div className="cart-product__container">
+                        <p className="cart-product__title">{title}</p>
+                        <p className="cart-product__price">Price: ${price}</p>
+                        <p className="cart-product__price">Quantity: {quantity}</p>
+                        <p className="cart-product__price">Total: ${price * quantity}</p>
+                    </div>
+                </div>
+            </div>}
         </div>
 )}
 
