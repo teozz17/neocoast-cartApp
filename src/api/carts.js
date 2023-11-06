cb0294716055929c077184ae97000e6c27ae0471
Mateo Barbachan <112145980+teozz17@users.noreply.github.com>
2023-11-05 22:03:47 -0300

    Revert "Revert "api call""
    This reverts commit c0583edd9dcd282ae9f490324de374578fa99871.

--- src/api/carts.js
+++ src/api/carts.js
@@ -4,6 +4,8 @@ const getCarts = () => instance.get('/carts');
 
 const getUserCart = (id) => instance.get(`/carts/user/${id}`);
 
+const getUserCartByCartId = (cart_id) => instance.get(`/carts/${cart_id}`);
+
 const setUserCart = (cart_id) => instance.post(`/carts/${cart_id}/buy`);
 
-export {getUserCart, getCarts, setUserCart};
+export {getUserCart, getCarts, setUserCart, getUserCartByCartId};
